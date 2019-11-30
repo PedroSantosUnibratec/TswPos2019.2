d1b5fcfb097453e1559c1c18ae9158b6ebee3390
pedropk <pedropk@gmail.com>
2019-11-30 15:29:38 -0300

    Exceções implementadas e salvas.

---
+++ PosTestes2019l2/src/main/java/atv01/ExcecaoDivisorIgualZero.java
@@ -0,0 +1,10 @@
+package atv01;
+
+public class ExcecaoDivisorIgualZero extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7311958616337953662L;
+
+}
---
+++ PosTestes2019l2/src/main/java/atv01/ExcecaoFormatoNumericoInvalido.java
@@ -0,0 +1,10 @@
+package atv01;
+
+public class ExcecaoFormatoNumericoInvalido extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5308046508212408269L;
+
+}
